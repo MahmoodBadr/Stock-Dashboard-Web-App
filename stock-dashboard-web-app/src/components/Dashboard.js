09ccbeeb323dca6f3f523677bc0d2247598a9adc
Mahmood Badr <mahmoodbadr@outlook.com>
2024-06-24 17:18:44 -0400

    Working Search Function

--- stock-dashboard-web-app/src/components/Dashboard.js
+++ stock-dashboard-web-app/src/components/Dashboard.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Card from "./Card";
+import { mockCompanyDetails } from "../constants/mock";
+import Header from "./Header";
+import Details from "./Details";
 
 const Dashboard = () => {
     return (
         <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand">
-            <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1">
-                <Card>Header</Card>
+            <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
+                <Header name={mockCompanyDetails.name}/>
             </div>
             <div className="md:col-span-2 row-span-4">
                 <Card>Chart</Card>
@@ -14,7 +17,7 @@ const Dashboard = () => {
                 <Card>Overview</Card>
             </div>
             <div className="row-span-2 xl:row-span-3">
-                <Card>Details</Card>
+                <Details details={mockCompanyDetails}/>
             </div>
         </div>
     );
