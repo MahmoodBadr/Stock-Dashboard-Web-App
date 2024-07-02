b15803f40fad90452a6294190fb152387d6cd1c6
Mahmood Badr <mahmoodbadr@outlook.com>
2024-07-02 18:30:34 -0400

    Finalizing Update

--- stock-dashboard-web-app/src/App.js
+++ stock-dashboard-web-app/src/App.js
@@ -1,13 +1,18 @@
-import { useState } from 'react';
-import './App.css';
-import Dashboard from './components/Dashboard';
-import ThemeContext from './context/ThemeContext';
+import { useState } from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import StockContext from "./context/StockContext";
+import ThemeContext from "./context/ThemeContext";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+  const [stockSymbol, setStockSymbol] = useState("MSFT");
+
   return (
-    <ThemeContext.Provider value={{ darkMode, setDarkMode}}>
-      <Dashboard />
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      <StockContext.Provider value={{ stockSymbol, setStockSymbol }}>
+        <Dashboard />
+      </StockContext.Provider>
     </ThemeContext.Provider>
   );
 }
--- stock-dashboard-web-app/src/components/Header.js
+++ stock-dashboard-web-app/src/components/Header.js
@@ -3,15 +3,15 @@ import Search from "./Search";
 import ThemeIcon from "./ThemeIcon";
 
 const Header = ({ name }) => {
-    return(
-        <>
-            <div className="xl:px-32">
-                <h1 className="text-5xl">{name}</h1>
-                <Search/>
-            </div>
-            <ThemeIcon/>
-        </>
-    );
+  return (
+    <>
+      <div className="xl:px-32">
+        <h1 className="text-5xl">{name}</h1>
+        <Search />
+      </div>
+      <ThemeIcon />
+    </>
+  );
 };
 
 export default Header;
